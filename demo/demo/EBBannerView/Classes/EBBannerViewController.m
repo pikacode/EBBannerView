67b8e8f28fde5bde0a4669410ef64635f59577c5
pikacode <pikacode@qq.com>
2017-10-25 19:03:11 +0800

    update demo

--- demo/demo/EBBannerView/Classes/EBBannerViewController.m
+++ demo/demo/EBBannerView/Classes/EBBannerViewController.m
@@ -8,32 +8,11 @@
 
 #import "EBBannerViewController.h"
 
-@interface EBBannerControllerView : UIView
-
-@end
-
-@implementation EBBannerControllerView
--(UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event{
-    if ([super hitTest:point withEvent:event]) {
-        return [super hitTest:point withEvent:event];
-    }else{
-        return self;
-    }
-}
-@end
-
-
 @interface EBBannerViewController ()
 
 @end
 
 @implementation EBBannerViewController
-
--(void)viewDidLoad{
-    [super viewDidLoad];
-    self.view = [EBBannerControllerView new];
-    self.view.frame = UIScreen.mainScreen.bounds;
-}
 
 -(UIInterfaceOrientationMask)supportedInterfaceOrientations{
     return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscape;
